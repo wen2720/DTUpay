ac8bea4e2f57d0a0e5e56b98c144099c241f223a
wen2720 <wenhao2720@gmail.com>
2022-08-09 13:44:29 +0200

    # spring # org.springframework.web.bind.annotation.* #  @RestController @RequestMapping, etc.

--- src/main/java/dtupay/App.java
+++ src/main/java/dtupay/App.java
@@ -5,6 +5,13 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
 @RestController
 @EnableAutoConfiguration
 public class App {
@@ -14,8 +21,35 @@ public class App {
         return "Hello World!";
     }
 
+    private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
     public static void main(String[] args) throws Exception {
         SpringApplication.run(App.class, args);
+        //SpringApplication.run(GreetingController.class, args);
     }
 
+}
+
+class Greeting {
+
+	private final long id;
+	private final String content;
+
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
 }
